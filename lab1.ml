318fbbf92174565b39302fd34a5979f36d3e52bb
Alex Gibbons <alexgibbons@Alexs-MacBook-Pro.local>
2018-02-01 14:27:00 -0500

    end up lab 1

--- lab1.ml
+++ lab1.ml
@@ -42,7 +42,7 @@ exercise2 is a function that returns 42 (instead of failing). When you
 submit, the Exercise 2 unit test should then pass.
 ......................................................................*)
 
-let exercise2 () = failwith "exercise2 not implemented" ;;
+let exercise2 () = 42 ;;
 
 (* Note that your grade on labs is not dependent on your passing the
 unit tests. You'll see that even when a unit test is marked as passed,
@@ -99,7 +99,7 @@ appropriate OCaml expression to assign the value to the variable
 exercise1 below.
 ......................................................................*)
 
-let exercise3 () = failwith "exercise3 not implemented" ;;
+let exercise3 () = - (5 - 3) ;;
 
 (* Hint: The OCaml concrete expression
 
@@ -111,9 +111,15 @@ does *not* correspond to the abstract syntax above.
 Exercise 4: Draw the tree that the concrete syntax "- 5 - 3" does
 correspond to. Check it with a member of the course staff if you'd
 like.
-......................................................................*)
+......................................................................
 
-   
+      -
+      ^
+     / \
+    /   \
+   -5    3
+
+   *)
 (*======================================================================
 Part 2: Types and type inference
 
@@ -124,24 +130,22 @@ expressions below? Test your solution by uncommenting the examples
 error is generated.
 ......................................................................*)
 
-(*   <--- remove this start of comment line
+let exercise5a : int = 42 ;;
 
-let exercise5a : ??? = 42 ;;
-
-let exercise5b : ??? =
+let exercise5b : string =
   let greet y = "Hello " ^ y
   in greet "World!";;
 
-let exercise5c : ???  =
+let exercise5c : (int * float) -> int  =
   fun (x, y) -> x + int_of_float y ;;
 
-let exercise5d : ??? =
+let exercise5d : (int) -> bool =
   fun x -> x < x + 1 ;;
 
-let exercise5e : ??? =
+let exercise5e : (bool) -> bool list =
   fun x -> if x then [x] else [] ;;
 
-remove this end of comment line too ----> *)
+
 
 (*======================================================================
 Part 3: First-order functional programming
@@ -170,18 +174,23 @@ to the list containing the elements 3, 4, and 5? You'll want to
 replace the "[]" with the correct functional call.
 ......................................................................*)
 
-let square_all (lst : int list) : int list =
-  failwith "square_all not implemented" ;;
+let rec square_all (lst : int list) : int list =
+  match lst with
+  | [] -> []
+  | hd::tl -> hd * hd :: square_all(tl) ;;
 
-let exercise6 = [] ;;
+let exercise6 = square_all [3; 4; 5] ;;
+
 
 (*......................................................................
 Exercise 7: Define a recursive function that sums an integer
 list. (What's a sensible return value for the empty list?)
 ......................................................................*)
 
-let sum (lst : int list) : int =
-  failwith "sum not implemented" ;;
+let rec sum (lst : int list) : int =
+  match lst with
+  | [] -> 0
+  | head :: tail -> head + sum tail ;;
   
 (*......................................................................
 Exercise 8: Define a recursive function that returns the maximum
@@ -190,8 +199,12 @@ can raise an appropriate exception -- a Match_failure or
 Invalid_argument exception for instance.
 ......................................................................*)
 
-let max_list (lst : int list) : int =
-  failwith "max_list not implemented" ;;
+let rec max_list (lst : int list) : int =
+  match lst with
+  | [] -> Match_failure("")
+  | hd::[] -> hd
+  | hd::tl -> if hd < max_list tl then hd else max_list tl;;
+  (*if hd < tl[0] then max_list tl else max_list hd::tl[1:];;
 
 (*......................................................................
 Exercise 9: Define a function zip, that takes two int lists and
